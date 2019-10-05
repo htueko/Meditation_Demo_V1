39de44f977959ee44e7ee99b683739f20b028c33
Htue Ko <htooko1101@gmail.com>
2019-10-06 00:12:25 +0630

    get respond from graphql server

--- app/src/main/java/com/example/meditationdemov1/MainActivity.kt
+++ app/src/main/java/com/example/meditationdemov1/MainActivity.kt
@@ -8,6 +8,8 @@ import com.apollographql.apollo.ApolloCall
 import com.apollographql.apollo.api.Response
 import com.apollographql.apollo.exception.ApolloException
 import com.example.meditationdemov1.model.network_client.MyApolloClient
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.StringBuilder
 
 class MainActivity : AppCompatActivity() {
 
@@ -29,7 +31,18 @@ class MainActivity : AppCompatActivity() {
 
             override fun onResponse(response: Response<FeedQueryResultQuery.Data>) {
                Log.d("Success Respond", "${response.data()}")
-
+                val sb = StringBuilder()
+                runOnUiThread {
+                    val characters = response.data()?.characters()
+                    val character = response.data()?.characters
+                    val operationName = response.operation().name()
+                    val data = response.data()
+                    sb.append(characters)
+                    sb.append(character)
+                    sb.append(operationName)
+                    sb.append(data)
+                    txt_placeholder_main.text = sb
+                }
             }
 
         })
